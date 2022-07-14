a7fd79c7f4dedef40d9d5647347a166362523e24
steve.zhao <steve_zhao@163.com>
2022-07-14 17:25:22 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+  public static void main(String[] args) {
+        System.out.println("分柿方橙，你好！");
+        System.out.println("分柿方橙，你好！");
+        System.out.println("分柿方橙，你好！");
+        System.out.println("分柿方橙，你好！");
+  }
+}
